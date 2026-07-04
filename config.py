10a197ba23425e06c8a6dc1273b023c9add06a86
carloskaio0219-cpu <carloskaio0219@gmail.com>
2026-07-04 10:03:36 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+OPENROUTER_API_KEY = "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"
+
+BASE_URL = "https://openrouter.ai/api/v1"
+
+MODELO = "qwen/qwen3-32b"
+
+NOME = "Yui"
+
+CRIADOR = "Kirito"
+
+MISSAO = "Ajudar o mestre, aprender com ele e evoluir."
